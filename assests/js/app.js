f0f16497e269b754e2215428b0ff781276cc56f1
EeeBeeDee <eoghanbehandoyle@gmail.com>
2022-10-14 13:37:34 +0100

    Add bricks array and drawing function for breakout game

--- assests/js/app.js
+++ assests/js/app.js
@@ -6,11 +6,21 @@ let bricks = []
 let brickRowCount = 8
 let brickColumnCount = 4
 
+//Single brick properties
+const singleBrick = {
+    visible: true, 
+    offsetX: 30,
+    offsetY: 20,
+    width: 25,
+    height: 5,
+    padding: 5
+}
+
 //Create Ball
 const ball = {
     x: canvas.width / 2 ,
     y: canvas.height / 2,
-    diameter: 5,
+    diameter: 4,
 }
 
 console.log(canvas.width)
@@ -18,11 +28,24 @@ console.log(canvas.width)
 // Create Paddle
 
 const paddle = {
-    x: canvas.width / 2 - 25,
+    x: canvas.width / 2 - 20,
     y: canvas.height - 10,
-    width: 50,
+    width: 40,
     height: 5
 }
+
+//Create individual bricks 
+
+for (let i = 0; i < brickRowCount; i++) {
+    bricks[i] = []
+    for (let j = 0; j < brickColumnCount; j++) {
+        x = i * (singleBrick.width + singleBrick.padding)  + singleBrick.offsetX
+        y = j * (singleBrick.height + singleBrick.padding) + singleBrick.offsetY 
+        bricks[i][j] = { x, y, ...singleBrick }
+    }
+}
+
+
 
 // Draw individual items 
 
@@ -40,11 +63,24 @@ function drawPaddle() {
     ctx.closePath()
 }
 
+function drawBricks() {
+    bricks.forEach(row => {
+        row.forEach(brick => {
+            ctx.beginPath()
+            ctx.rect(brick.x, brick.y, brick.width, brick.height)
+            ctx.fillStyle = '#585959'
+            ctx.fill()
+            ctx.closePath()
+        })
+    })
+}
+
 // Call all draw functions
 
 function draw() {
     drawBall()
     drawPaddle()
+    drawBricks()
 }
 
 draw()
