6770b93d6c9de4f89841377a72136580b7427a2f
EeeBeeDee <eoghanbehandoyle@gmail.com>
2022-10-25 22:09:50 +0100

    Add win/lose screen and score to quiz

--- assests/js/quiz.js
+++ assests/js/quiz.js
@@ -1,20 +1,24 @@
+/* jshint esversion: 11 */
+
+
 const questionEl = document.getElementById('question')
 const answersEl = document.getElementById('answers')
+const winLoseScreen = document.getElementById('win-lose-screen')
 let answerBtns 
 
 const URLArr = [
-    'https://opentdb.com/api.php?amount=5&category=15&difficulty=easy&type=multiple',
-    'https://opentdb.com/api.php?amount=5&category=15&difficulty=medium&type=multiple',
-    'https://opentdb.com/api.php?amount=5&category=15&difficulty=hard&type=multiple',
-    'https://opentdb.com/api.php?amount=5&category=18&difficulty=medium&type=multiple',
-    'https://opentdb.com/api.php?amount=5&category=18&difficulty=hard&type=multiple'
+    'https://opentdb.com/api.php?amount=10&category=15&difficulty=easy&type=multiple',
+    'https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple',
+    'https://opentdb.com/api.php?amount=10&category=15&difficulty=hard&type=multiple',
+    'https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=multiple',
+    'https://opentdb.com/api.php?amount=10&category=18&difficulty=hard&type=multiple'
 ]
 
 let quizQuestions 
 
 let currentQuestion = 0
 let correctAnswer 
-let score = 0
+let quizScore = 0
 
 function shuffleArr(arr) {
     for (let i = 0; i < arr.length; i++) {
@@ -67,19 +71,59 @@ function checkAnswer(e) {
         questionEl.innerText = 'Correct!'
         answersEl.innerHTML = ''
         currentQuestion += 1
-        score += 1
-        setTimeout(getQuiz, 2000)
+        quizScore += 1
+        if (currentQuestion < 10) {
+        setTimeout(getQuiz, 1000)
+        }  else  {
+            console.log('hello')
+            setTimeout(checkWinLose, 1000)
+        }
     } else {
         console.log('incorrect')
         questionEl.innerText = 'incorrect X'
         answersEl.innerHTML = ''
         currentQuestion += 1
-        score += 0
-        setTimeout(getQuiz, 2000)
+        quizScore += 0
+        if (currentQuestion < 5) {
+            setTimeout(getQuiz, 1000)
+        } else  {
+            console.log('hello')
+            setTimeout(checkWinLose, 1000)
+        }
     }
 }
 
-   
+console.log(currentQuestion)
+
+function checkWinLose() {
+    if (currentQuestion === 10 && quizScore >= 6) {
+        totalScore = totalScore + (scoreMultiplier * 100)
+            scoreMultiplier += 1
+            difficulty += 1
+            winLoseScreen.style.display = 'flex'
+            winLoseScreen.innerHTML = `
+            <h2>Congrats you've won!</h2>
+            <h2>Current Score : ${totalScore}</h2>
+            <h2>Current current multiplier : x${scoreMultiplier}</h2>
+            <h2> choose your next game or retstart from scratch! </h2>
+            <a class="restart-btn" href="index.html">Restart Gauntlet</a>
+            <div class="crt crt-overlay"></div>
+            `
+        
+    } else {
+        scoreMultiplier = 1
+        difficulty = 1
+        winLoseScreen.style.display = 'flex'
+        winLoseScreen.innerHTML = `
+        <h2>Your Score multiplyer and difficulty have been reset!</h2>
+        <h2>Current Score : ${totalScore}</h2>
+        <h2>Current current multiplier : x${scoreMultiplier}</h2>
+        <h2> choose your next game or retstart from scratch! </h2>
+        <a class="restart-btn" href="index.html">Restart Gauntlet</a>
+        <div class="crt crt-overlay"></div>
+        `
+    }
+}
 
 getQuiz()
 
